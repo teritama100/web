530900094e1781584edb7556a804ca0bac9f5042
teritama100 <hainosyo@gmail.com>
2025-06-18 03:46:25 +0900

    Initial commit

---
+++ stydy.js
@@ -0,0 +1,12 @@
+                    let user={
+                        name:"灰野　翔",
+                        age:26,
+                        like:"絵を描くこと",
+                        dream:"大きな人間になること",
+                    
+                        talk:function () {
+                            console.log('私の名前は'+user.name+'です。',
+                                '年齢は'+user.age+'です。',
+                                '好きなことは'+user.like+'です。',
+                                '将来の夢は'+user.dream+'です。')
+                        }}
